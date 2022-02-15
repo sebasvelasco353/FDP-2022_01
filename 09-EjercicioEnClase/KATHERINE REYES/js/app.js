539c4c49ee8b7bd1e2ff55a6a9ddab85f17bdd39
Kathereyes19 <98465512+Kathereyes19@users.noreply.github.com>
2022-02-15 17:21:38 -0500

    creo que termine el html y css. Inicie con el js
    termine el html y css, si es que no me da la locura de cambiar algo otra vez. Empece a hacer el java

--- 09-EjercicioEnClase/KATHERINE REYES/js/app.js
+++ 09-EjercicioEnClase/KATHERINE REYES/js/app.js
@@ -1,12 +1,27 @@
-var input = document.getElementsByClassName("formulario_input");
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].addElementsListener("keyput" , function() {
-    if(this.valuelength>=1) {
-       this.nextElementsSibing.classList.add("fijar");
-    }  else {
-       this.nextElementsSibing.classList.remove("fijar");
-        }
-    });
+ var Nombre = document.getElementById("Nombre").value;
+ console.log(Nombre);
+
+ var Apellido = document.getElementById("Apellido").value;
+ console.log(Apellido);
+
+ var Estatura = document.getElementById("Estatura").value;
+ console.log(Estatura);
+
+ var Fumador = document.getElementById("Fumador").value;
+ console.log(Fumador);
+
+ var Edad = document.getElementById("Edad").value;
+ console.log(Edad);
+
+ var Nacionalidad = document.getElementById("Nacionalidad").value;
+ console.log(Nacionalidad);
+
+ var RH = document.getElementById("RH").value;
+ console.log(RH);
+
+ var Cirugías = document.getElementById("Cirugías").value;
+ console.log(Cirugías);
+
 
 const paises = ['Afganistán','Albania','Alemania','Andorra','Angola','Antigua y Barbuda','Arabia Saudita','Argelia','Argentina','Armenia','Australia','Austria','Azerbaiyán','Bahamas','Bangladés','Barbados','Baréin','Bélgica','Belice','Benín','Bielorrusia','Birmania','Bolivia','Bosnia y Herzegovina','Botsuana','Brasil','Brunéi','Bulgaria','Burkina Faso','Burundi','Bután','Cabo Verde','Camboya','Camerún','Canadá','Catar','Chad','Chile','China','Chipre','Ciudad del Vaticano','Colombia','Comoras','Corea del Norte','Corea del Sur','Costa de Marfil','Costa Rica','Croacia','Cuba','Dinamarca','Dominica','Ecuador','Egipto','El Salvador','Emiratos Árabes Unidos','Eritrea','Eslovaquia','Eslovenia','España','Estados Unidos','Estonia','Etiopía','Filipinas','Finlandia','Fiyi','Francia','Gabón','Gambia','Georgia','Ghana','Granada','Grecia','Guatemala','Guyana','Guinea','Guinea ecuatorial','Guinea-Bisáu','Haití','Honduras','Hungría','India','Indonesia','Irak','Irán','Irlanda','Islandia','Islas Marshall','Islas Salomón','Israel','Italia','Jamaica','Japón','Jordania','Kazajistán','Kenia','Kirguistán','Kiribati','Kuwait','Laos','Lesoto','Letonia','Líbano','Liberia','Libia','Liechtenstein','Lituania','Luxemburgo','Madagascar','Malasia','Malaui','Maldivas','Malí','Malta','Marruecos','Mauricio','Mauritania','México','Micronesia','Moldavia','Mónaco','Mongolia','Montenegro','Mozambique','Namibia','Nauru','Nepal','Nicaragua','Níger','Nigeria','Noruega','Nueva Zelanda','Omán','Países Bajos','Pakistán','Palaos','Palestina','Panamá','Papúa Nueva Guinea','Paraguay','Perú','Polonia','Portugal','Reino Unido','República Centroafricana','República Checa','República de Macedonia','República del Congo','República Democrática del Congo','República Dominicana','República Sudafricana','Ruanda','Rumanía','Rusia','Samoa','San Cristóbal y Nieves','San Marino','San Vicente y las Granadinas','Santa Lucía','Santo Tomé y Príncipe','Senegal','Serbia','Seychelles','Sierra Leona','Singapur','Siria','Somalia','Sri Lanka','Suazilandia','Sudán','Sudán del Sur','Suecia','Suiza','Surinam','Tailandia','Tanzania','Tayikistán','Timor Oriental','Togo','Tonga','Trinidad y Tobago','Túnez','Turkmenistán','Turquía','Tuvalu','Ucrania','Uganda','Uruguay','Uzbekistán','Vanuatu','Venezuela','Vietnam','Yemen','Yibuti','Zambia','Zimbabue'];
 const grupoSanguineo = ['A+', 'B+', 'O+', 'AB+', 'A-', 'B-', 'O-', 'AB-'];
