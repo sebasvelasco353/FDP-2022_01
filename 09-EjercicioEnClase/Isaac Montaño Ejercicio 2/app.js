c938d4b63c0562c08af5e8c70c0c8d095da92ec3
Kazura-Collins <98463668+Kazura-Collins@users.noreply.github.com>
2022-02-14 23:26:19 -0500

    Add files via upload
    ok he avanzado un buen, pero aún no me sirve nada aaaaaaa

--- 09-EjercicioEnClase/Isaac Montaño Ejercicio 2/app.js
+++ 09-EjercicioEnClase/Isaac Montaño Ejercicio 2/app.js
@@ -16,6 +16,23 @@ for (let i = 0; i < grupoSanguineo.length ; i++) {
     document.getElementById("GrupoSanguineo").appendChild(option);
     }
 
+    const edades = ['', '1' ,	'2',	'3',	'4',	'5',	'6',	'7'	,'8',	'9',	'10',
+'11',	'12'	,'13'	,'14',	'15',	'16',	'17',	'18',	'19',	'20',
+'21',	'22',	'23',	'24'	,'25',	'26',	'27',	'28',	'29',	'30',
+'31',	'32',	'33',	'34',	'35',	'36',	'37',	'38',	'39',	'40',
+'41',	'42',	'43',	'44',	'45',	'46',	'47',	'48',	'49',	'50',
+'51',	'52',	'53',	'54',	'55',	'56',	'57',	'58',	'59',	'60',
+'61',	'62',	'63',	'64',	'65',	'66',	'67',	'68',	'69',	'70',
+'71',	'72', '73',	'74',	'75',	'76',	'77',	'78',	'79',	'80',
+'81',	'82',	'83',	'84',	'85',	'86',	'87',	'88',	'89',	'90',
+'91',	'92',	'93',	'94',	'95',	'96',	'97',	'98',	'99',	'100'];
+for (let i = 0; i < edades.length ; i++) {
+    const tempObj = edades[i];
+    var option = document.createElement("option");
+    option.text= tempObj
+    option.value=tempObj
+    document.getElementById("edad").appendChild(option);
+    }
 
 function datos() {
     var Nombre = document.getElementById ("Nombre").value;
@@ -26,7 +43,7 @@ function datos() {
     var Nacionalidad = document.getElementById ("nacionalidad").value;
     var Sangre = document.getElementById ("GrupoSanguineo").value;
     var Cirugias = document.getElementById ("cirugias").value;
-}
+
 {
     console.log ("Nombre: " + Nombre )
     console.log ("Apellido: " + Apellido) 
@@ -37,6 +54,17 @@ function datos() {
     console.log ("Tipo de sangre: " + Sangre)
     console.log ("Cirugias que ha tenido: " + Cirugias)
 
+}}
+
+function DisplayAnciano() {
+    if (document.getElementById("edad").value >= "50" )
+    { document.getElementById("Anciano").style.display = 'block'
+}else{document.getElementById("Anciano").style.display = "none"; 
 }
-
-
+}
+function DisplayJoven() {
+    if (document.getElementById("edad").value < "50" )
+    { document.getElementById("Joven").style.display = 'block'
+}else{document.getElementById("Joven").style.display = "none"; 
+}
+}
