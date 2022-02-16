38e2346ef6b76b4ddfbd5cd53e4f6799ee6e8ecc
Juliana <56936845+Juli0931@users.noreply.github.com>
2022-02-16 00:20:42 -0500

    Cambié los <p> por <label>, incorporé ambos dopdrown y relicé las condiciones, kinda

--- 09-EjercicioEnClase/Lab1102 - Juliana/main.js
+++ 09-EjercicioEnClase/Lab1102 - Juliana/main.js
@@ -12,30 +12,49 @@
 
 // Usen estas constantes para llenar los campos de seleccion.
 
-var Nombre = document.getElementById("Nombre").value;
-console.log(Nombre);
-
-var Apellido = document.getElementById("Apellido").value;
-console.log(Apellido);
-
-var Estatura = document.getElementById("Estatura").value;
-console.log(Estatura);
-
-var Fumador = document.getElementById("Fumador").value;
-console.log(Fumador);
-
-var Edad = document.getElementById("Edad").value;
-console.log(Edad);
-
-var Nacionalidad = document.getElementById("Nacionalidad").value;
-console.log(Nacionalidad);
-
-var RH = document.getElementById("RH").value;
-console.log(RH);
-
-var Cirugías = document.getElementById("Cirugías").value;
-console.log(Cirugías);
-
 const paises = ['Afganistán','Albania','Alemania','Andorra','Angola','Antigua y Barbuda','Arabia Saudita','Argelia','Argentina','Armenia','Australia','Austria','Azerbaiyán','Bahamas','Bangladés','Barbados','Baréin','Bélgica','Belice','Benín','Bielorrusia','Birmania','Bolivia','Bosnia y Herzegovina','Botsuana','Brasil','Brunéi','Bulgaria','Burkina Faso','Burundi','Bután','Cabo Verde','Camboya','Camerún','Canadá','Catar','Chad','Chile','China','Chipre','Ciudad del Vaticano','Colombia','Comoras','Corea del Norte','Corea del Sur','Costa de Marfil','Costa Rica','Croacia','Cuba','Dinamarca','Dominica','Ecuador','Egipto','El Salvador','Emiratos Árabes Unidos','Eritrea','Eslovaquia','Eslovenia','España','Estados Unidos','Estonia','Etiopía','Filipinas','Finlandia','Fiyi','Francia','Gabón','Gambia','Georgia','Ghana','Granada','Grecia','Guatemala','Guyana','Guinea','Guinea ecuatorial','Guinea-Bisáu','Haití','Honduras','Hungría','India','Indonesia','Irak','Irán','Irlanda','Islandia','Islas Marshall','Islas Salomón','Israel','Italia','Jamaica','Japón','Jordania','Kazajistán','Kenia','Kirguistán','Kiribati','Kuwait','Laos','Lesoto','Letonia','Líbano','Liberia','Libia','Liechtenstein','Lituania','Luxemburgo','Madagascar','Malasia','Malaui','Maldivas','Malí','Malta','Marruecos','Mauricio','Mauritania','México','Micronesia','Moldavia','Mónaco','Mongolia','Montenegro','Mozambique','Namibia','Nauru','Nepal','Nicaragua','Níger','Nigeria','Noruega','Nueva Zelanda','Omán','Países Bajos','Pakistán','Palaos','Palestina','Panamá','Papúa Nueva Guinea','Paraguay','Perú','Polonia','Portugal','Reino Unido','República Centroafricana','República Checa','República de Macedonia','República del Congo','República Democrática del Congo','República Dominicana','República Sudafricana','Ruanda','Rumanía','Rusia','Samoa','San Cristóbal y Nieves','San Marino','San Vicente y las Granadinas','Santa Lucía','Santo Tomé y Príncipe','Senegal','Serbia','Seychelles','Sierra Leona','Singapur','Siria','Somalia','Sri Lanka','Suazilandia','Sudán','Sudán del Sur','Suecia','Suiza','Surinam','Tailandia','Tanzania','Tayikistán','Timor Oriental','Togo','Tonga','Trinidad y Tobago','Túnez','Turkmenistán','Turquía','Tuvalu','Ucrania','Uganda','Uruguay','Uzbekistán','Vanuatu','Venezuela','Vietnam','Yemen','Yibuti','Zambia','Zimbabue'];
 const grupoSanguineo = ['A+', 'B+', 'O+', 'AB+', 'A-', 'B-', 'O-', 'AB-'];
 
+var NacionalidadSelect = document.getElementById("Nacionalidad");
+    for (let i = 0; i < paises.length; i++) {
+        const element = paises[i];
+        let htmlElement = document.createElement("option");
+        htmlElement.setAttribute("value", element);
+        htmlElement.innerHTML = element;
+        NacionalidadSelect.appendChild(htmlElement);
+    }
+
+var RHSelect = document.getElementById("RH");
+    for (let i = 0; i < grupoSanguineo.length; i++) {
+        const element = grupoSanguineo[i];
+        let htmlElement = document.createElement("option");
+        htmlElement.setAttribute("value", element);
+        htmlElement.innerHTML = element;
+        RHSelect.appendChild(htmlElement);
+    }
+
+const handleClick = (e) => { 
+        const obj = {
+            Nombre: document.getElementById("Nombre").value,
+            Apellido: document.getElementById("Apellido").value,
+            Estatura: document.getElementById("Estatura").value,
+            Fumador: document.getElementById("Fumador").checked,
+            Edad: document.getElementById("Edad").value,
+            Nacionalidad: document.getElementById("Nacionalidad").value,
+            RH: document.getElementById("RH").value,
+            Cirugías: document.getElementById("Cirugías").checked,
+        }
+    
+        var registroDatos = [];
+        
+        registroDatos.push(obj);
+        console.log(registroDatos);
+    }
+
+const AnotherQ = (f) => {
+        if (document.getElementById("Edad").value >= 50) {
+            document.getElementById("ColesterolQ").style.display="block";
+        } else {
+            document.getElementById("AzucarQ").style.display="block";
+        }
+    }
