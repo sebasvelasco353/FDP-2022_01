196ed87644e244934eeda8d0a640b86a099974bd
Kathereyes19 <98465512+Kathereyes19@users.noreply.github.com>
2022-02-24 12:46:40 -0500

    agregue el css y empece js
    me di cuenta que por alguna extraña razón no se había subido el css entonces aquí esta.

---
+++ 12-EjercicioMetodos/KATHERINE REYES/KATHERINE REYES /js/app.js
@@ -0,0 +1,12 @@
+const mostrarreceta = (receta) => {
+    console. log(receta);
+    switch (receta) {
+        case "receta1": 
+        console.log("mostrar1");
+        break;
+        case "receta2": 
+        console.log("mostrar2");
+        default:
+            break;
+    }
+}
