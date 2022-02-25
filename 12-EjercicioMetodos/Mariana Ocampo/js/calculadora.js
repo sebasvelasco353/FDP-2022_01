ed808e6f9dd8fba1a51f523fee9e47c59c898afc
Marianaao <98611561+Marianaao@users.noreply.github.com>
2022-02-25 13:38:15 -0500

    Calculadora casi terminada, con problemas de impresion en el archivo js

---
+++ 12-EjercicioMetodos/Mariana Ocampo/js/calculadora.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', ()=> {
+  
+ /* creo constantes para guardar elementos que necesito */
+    const display = document.querySelector('.calculator-display');
+    const keypadButtons = document.getElementsByClassName('keypad-button');
+
+    /* convierto el html a Array */
+    const keypadButtonsArray = Array.from(keypadButtons);
+
+    /* Itereamos nuevo Array de botones */
+    keypadButtonsArray.forEach( (button) => {
+       
+       /* Cada boton le agrego un listener*/
+        button.addEventListener('click', () => {
+            calculadora(button, display);
+
+        })
+    })
+});
+
+function calculadora(button, display) {
+    switch (button.innerHTML) {
+        case 'C':
+            borrar(display);
+            break;
+
+            case '=':
+                calcular(display);
+                break;
+
+                default:
+                    actualizar(display, button);
+                    break;
+    }
+}
+
+function calcular(display){
+    display.innerHTML = eval(display.innerHTML)
+
+}
+
+function actualizar(diplay, button){
+    if (display.innerHTML == 0){
+        display.innerHTML = '';
+    }
+    display.innerHTML += button.innerHTML;
+}
+
+function borrar(display){
+    display.innerHTML = 0;
+}
