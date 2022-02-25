00e084c7f625d2a6c8049033c31020e74d3f135c
AlejandroRv19 <98325607+AlejandroRv19@users.noreply.github.com>
2022-02-24 22:08:36 -0500

    Alejandro Rueda Venegas-Metodos
    Hice la actividad de la receta que aparece y desaparece. cuando se le da click.

---
+++ 12-EjercicioMetodos/Alejandro-progra/js/app.js
@@ -0,0 +1,20 @@
+function receta3()
+{document.getElementById('text').style.display = 'block';}
+
+function receta33()
+{document.getElementById('text').style.display = 'none';}
+
+
+function receta2()
+{document.getElementById('texto2').style.display = 'block';}
+
+function receta22()
+{document.getElementById('texto2').style.display = 'none';}
+
+
+
+function receta1()
+{document.getElementById('texto1').style.display = 'block';}
+
+function receta11()
+{document.getElementById('texto1').style.display = 'none';}
