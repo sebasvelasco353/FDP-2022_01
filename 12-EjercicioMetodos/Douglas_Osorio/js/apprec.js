2ad4314e1b19cf53d70ddd2bc7ee0d29cd6337bb
DarkSea1220 <98364095+Darksea1220@users.noreply.github.com>
2022-02-24 19:42:57 -0500

    Adjunto el mvp completamente funcional, creo que se puede optimizar más pero mis conocimientos no llegan más allá, tiene 4 métodos de los que uno recibe y regrsa un valor,además es aprueba de <<tontos>> puesdes intentar ingresar un valor negativo en la cantidad de porciones =P

--- 12-EjercicioMetodos/Douglas_Osorio/js/apprec.js
+++ 12-EjercicioMetodos/Douglas_Osorio/js/apprec.js
@@ -1,3 +1,7 @@
+var ant="";
+var fid="";
+var antibug=0;
+
 const baseing = () => {
 let base = document.getElementById("recetario");
 base.innerText =`-Zanahoria.
@@ -17,26 +21,67 @@ const baseproc = () => {
 const guiso = () =>{
     let carne = document.getElementById("carne")
     carne.innerText =`Para complementar puedes preparar un guiso.
-            empieza agregando recortes de carne o carne molida en una sartén con un poco de aceite pre-calentado, pica una cebolla y un tomate, agrégaselos a la carne, deja cocinar a fuego rápido unos treinta minutospuedes agregar salsa para carnes y a comer.`;
+    
+            empieza agregando recortes de carne o carne molida en una sartén con un poco de aceite pre-calentado, pica una cebolla y un tomate, agrégaselos a la carne, deja cocinar a fuego rápido unos treinta minutos, puedes agregar salsa para carnes y a comer.`;
 }
 const cocina = () =>{
-    var princ = document.getElementById("princ")
-    if (princ = "fideos") {
-        let ant = document.getElementById("default");
+    var princ = document.getElementById("princ").value;
+    if (princ === "fideos") {
+        ant = document.getElementById("default");
         ant.innerText = "Además de los fideos necesitarás:";
         baseing ();
-        let fid = document.getElementById("unic");
-        fid.innerText="Añade los fideos junto con la zanahoria y el zapallo, espera diez minutos. Está listo.";
+        fid = document.getElementById("unic");
+        fid.innerText="Añade los fideos junto con la zanahoria y el zapallo, espera de seis a nueve minutos. Está listo.";
         guiso();
-    } else {if (princ = "avena") {
-        let ant = document.getElementById("default");
+        antibug=1;
+    } else if (princ === "avena") {
+        ant = document.getElementById("default");
         ant.innerText = "Además de la avena necesitarás:";
         baseing ();
-        let fid = document.getElementById("unic");
-        fid.innerText='Agrega la zanahoria y el zapallo, deja cocinar unos 5 minutos mientras tuestas la avena en una sartén a fuego rápido, añande la avena, espera entre veinticinco y treinta minutos. Está listo.';
+        fid = document.getElementById("unic");
+        fid.innerText='Agrega la zanahoria y el zapallo, deja cocinar unos dos minutos mientras tuestas la avena en una sartén a fuego rápido, una vez tostada añande la avena, espera entre diez y veinte minutos. Está listo.';
         guiso();
-            } else {
-                baseing ();
+        antibug=2
+    } else {
+        ant = document.getElementById("default");
+        ant.innerText = "Además del cuchuco de maíz necesitarás:";
+        baseing ();
+        fid = document.getElementById("unic");
+        fid.innerText='Añade la zanahoria, el zapallo y el cuchuco de maíz, deja hervir unos diez minutos. Está listo.';
+        guiso();
+        antibug=3;
     }     
+}   
+const precant = () =>{
+    var porcion= document.getElementById("porcion").value;
+let ingcanti = document.getElementById("ingcant");
+    var zana = 1 * porcion;
+    var zapa= 200 * porcion;
+    var papa= 1 * porcion;
+    ingcanti.innerText =`la cantidad aproximada de ingredientes necesariospara cocinar ${porcion} porcion(es) son:
+    - Zanahoria(s): ${zana}
+    - Zapallo: ${zapa}g
+    - Papa(s): ${papa}
+    - Condimentos al gusto`;
+}
+const showcant = () =>{
+    var porcion2 = document.getElementById("porcion").value;
+    if(porcion2<1){
+        var cero = document.getElementById("ingcant2")
+        cero.innerText =`Hacete nomás, cómo querés descocinar???.`
+    } 
+    else if (antibug===1) {
+        precant();
+        var uno = document.getElementById("ingcant2")
+        uno.innerText =`Y ${porcion2*70}g de fideos.`
+    } else if(antibug===2) {
+        precant();
+        var dos = document.getElementById("ingcant2")
+        dos.innerText =`Y ${porcion2*60}g de avena.`
+    }
+    else if (antibug===3){
+        precant();
+        var tres = document.getElementById("ingcant2")
+        tres.innerText =`Y ${porcion2*80}g de cuchuco de maíz.`
     }
 }
