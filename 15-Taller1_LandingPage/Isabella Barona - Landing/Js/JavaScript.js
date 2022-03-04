d68743e7eaaf7ab4f5268aa191d88166730c807b
Isabella Barona <57952250+IBaronap@users.noreply.github.com>
2022-03-04 11:20:05 -0500

    ..

--- 15-Taller1_LandingPage/Isabella Barona - Landing/Js/JavaScript.js
+++ 15-Taller1_LandingPage/Isabella Barona - Landing/Js/JavaScript.js
@@ -1,3 +1,4 @@
+//Animaciones
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.from('.header', { duration: 1, y: '-100%', ease: 'power3.inOut'})
@@ -18,7 +19,7 @@ gsap.from('.T2', { duration: 1.5, x:'-10%', opacity: '0', stagger:0.3, scrollTri
 gsap.from('.info', { duration: 1.5, x:'-10%', opacity: '0', stagger:0.3, scrollTrigger:'.info'})
 gsap.from('#Formulario', { duration: 1.5, x:'10%', opacity: '0', stagger:0.3, scrollTrigger:'.Formulario'})
 
-
+//Navegación de la página
 const links = document.querySelectorAll(".Nav_Link");
 
 links.forEach((item)=>{
@@ -29,6 +30,78 @@ links.forEach((item)=>{
     })
 })
 
+//Mensaje Popup de productos
+const openPopupButtons = document.querySelectorAll('[data-popup-target]')
+
+const closePopupButtons = document.querySelectorAll('[data-close-button]')
+
+const overlay = document.getElementById('overlay')
+
+openPopupButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        const popup = document.querySelector(button.dataset.popupTarget)
+        openPopup(popup)
+    })
+})
+
+closePopupButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        const popup = button.closest('.popup')
+        closePopup(popup)
+    })
+})
+
+function openPopup(popup){
+    if (popup == null) return
+    popup.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closePopup(popup){
+    if (popup == null) return
+    popup.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+//Mensaje popup de formulario
+const openPopupEButtons = document.querySelectorAll('[data-popupE-target]')
+
+const closePopupEButtons = document.querySelectorAll('[data-closeE-button]')
+
+openPopupEButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        const popupE = document.querySelector(button.dataset.popupETarget)
+        openPopupE(popupE)
+    })
+})
+
+closePopupEButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        const popupE = button.closest('.popupE')
+        closePopupE(popupE)
+    })
+})
+
+
+function openPopupE(popupE){
+    if (popupE == null) return
+    popupE.classList.add('active')
+    overlay.classList.add('active')
+    }
+
+function closePopupE(popupE){
+    if (popupE == null) return
+    popupE.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+document.getElementById("#Enviar").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+
+
+//Console log
 function datos() {
     var Nombre = document.getElementById("Nombre").value;
     var Teléfono = document.getElementById("Teléfono").value;
