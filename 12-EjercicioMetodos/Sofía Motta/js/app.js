11fb43f85db5e70a482d8efa0fa1b06f2477cd51
Yo <98538133+FOOSSIILL@users.noreply.github.com>
2022-03-04 08:56:36 -0500

    main

--- 12-EjercicioMetodos/Sofía Motta/js/app.js
+++ 12-EjercicioMetodos/Sofía Motta/js/app.js
@@ -1,19 +1,132 @@
-'use strict'
+var acc = document.getElementsByClassName("tortaNegra");
+  var i;
 
-const bloque = document.querySelectorAll('.bloque')
-const h2     =document.querySelectorAll('.h2')
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
 
+ var acc = document.getElementsByClassName("tortaNegraMateriales");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    }); 
+  }
 
-// Cuando damos CLICK en h2,
-    // QUITAR la clase activo de TODOS los bloques
-    //Vamos a añadir la clase activo al BLOQUE con la POSICIÓN DEL h2
+  var acc = document.getElementsByClassName("tortaNegraInstrucciones");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
 
-h2.forEach( (cadaH2 , i )=>{
-    h2[i].addEventListener ('click', ()=>{
+  var acc = document.getElementsByClassName("tamalesAntioquenos");
+  var i;
 
-        bloque.forEach ( (cadaBloque , i )=>{
-           bloque[i].classList.remove('activo') 
-        })
-        bloque[i].classList.add('activo')
-    })
-})
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
+  acc = document.getElementsByClassName("tamalesAntioquenosMateriales");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
+  var acc = document.getElementsByClassName("tamalesAntioquenosInstrucciones");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
+
+  var acc = document.getElementsByClassName("AlbondigaColombianas");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }  
+
+  acc = document.getElementsByClassName("AlbondigasColombianasMateriales");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
+
+  var acc = document.getElementsByClassName("AlbondigasColombianasInstrucciones");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
