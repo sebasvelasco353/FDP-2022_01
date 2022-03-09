0c92a31079f5f3d6141fba95c448b9fd203df466
salazar2356 <98467380+salazar2356@users.noreply.github.com>
2022-03-09 13:39:04 -0500

    solo falta el feedback del formulario

--- 15-Taller1_LandingPage/TALLER ISABELLA/app.js
+++ 15-Taller1_LandingPage/TALLER ISABELLA/app.js
@@ -8,11 +8,6 @@ window.addEventListener("scroll",function(){
 
 gsap.from('.container', {duration:1, x:'-100vw', delay: 0.5 , ease: 'power2.in'})
 
-/*FOOTER*/
-
-gsap.from('.footer', {duration:1, y:'-100%', ease:'bounce'})
-
-gsap.from('.elementos', {duration:0.5, y: '-100%', opacity:0, delay: 1, })
 
 /*FORMULARIO*/
 
@@ -23,6 +18,15 @@ gsap.from('.form-register', {duration:1, x:'-100vw', delay: 0.5, ease: 'power2.i
 gsap.from('.form', { duration:1, x:'-100vw', delay:0.5, ease:'power2.in'})
 
 gsap.from('.form__container', {duration:1.5, x:'-100%', delay: 0.5, ease:'power2.in'})
+
+/*FOOTER*/
+
+gsap.from('.footer', {duration:0.7, y:'-100%', ease:'bounce'})
+
+gsap.from('.elementos', {duration:0.3, y: '-100%', opacity:0, delay: 0.5, })
+
+gsap.from('.aboutus', {duration:2, y:'-100', opacity: 0, delay: 0.5,})
+
 
 
 
