c28a2891cd39552f0b69102a526a5579f2784c15
Kazura-Collins <98463668+Kazura-Collins@users.noreply.github.com>
2022-03-10 23:01:38 -0500

    Primer avance landing page
    Aún no hay menú arriba ni espacio abajo, solo cards sin bordes  o hover.

---
+++ 15-Taller1_LandingPage/Isaac Montaño Landing Page/java.js
@@ -0,0 +1,16 @@
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 100) {
+      document.querySelector('.go-top-container').classList.add('show');
+       
+    }
+    else{
+      document.querySelector('.go-top-container').classList.remove('show');
+    }
+  }
+   
+  document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
