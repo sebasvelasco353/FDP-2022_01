84c144de8231f9c51906f5969b65e22f12addca2
Kathereyes19 <98465512+Kathereyes19@users.noreply.github.com>
2022-03-10 23:16:08 -0500

    mejoras en el formulario y js
    rediseñe el concepto del formulario y cree la validación con js

--- 15-Taller1_LandingPage/Katherine Reyes/script.js
+++ 15-Taller1_LandingPage/Katherine Reyes/script.js
@@ -1,3 +1,5 @@
+/* header */
+
 let boton = document.getElementById("icono");
 let enlaces = document.getElementById("enlaces");
 let contador = 0;
@@ -11,5 +13,39 @@ boton.addEventListener("click", function(e){
         enlaces.classList.remove("dos")
         enlaces.className = ("enlaces uno")
         contador =0;
+    }
+})
+
+/* Validación Formulario */
+
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+    if(pass.value.length < 8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado correctamente"
     }
 })
