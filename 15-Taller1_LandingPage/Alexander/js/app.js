7dad235fd67b06bc634c2a0bd049a5776ccc87a9
Alexander <98567345+Axelito02@users.noreply.github.com>
2022-03-10 21:15:10 -0500

    taller landing alexander

---
+++ 15-Taller1_LandingPage/Alexander/js/app.js
@@ -0,0 +1,7 @@
+function comprobarcampos(){
+    nombre= document.getElementById('nombre').value;
+    telefono= document.getElementById('telefono').value;
+    correo= document.getElementById('correo').value;
+    mensaje= document.getElementById('mensaje').value;
+    console.log(nombre, telefono, correo, mensaje);
+}
