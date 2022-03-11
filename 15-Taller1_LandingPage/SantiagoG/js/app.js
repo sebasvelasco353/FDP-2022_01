319f3acd883b6483ea332922c0a26068773ad538
Santiago <98619680+Santygr21@users.noreply.github.com>
2022-03-11 14:38:54 -0500

    Agregué algunas interacciones en css y gsap, aunque no funcionan

---
+++ 15-Taller1_LandingPage/SantiagoG/js/app.js
@@ -0,0 +1,64 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".header", {
+    y: '-100%',
+    duration: 1.5,
+})
+
+gsap.from(".LogoHeader", {
+    y: '-15%',
+    duration: 2,
+    delay: 1
+})
+
+gsap.from(".DivNav", {
+    y:'-100%',
+    duration: 1.5,
+    ease: 'power3.inOut'
+})
+
+gsap.from(".DivBienvenida", {
+    y: '-15%',
+    duration: 2,
+    delay: 1,
+    scrollTrigger: ".DivBienvenida"
+})
+
+gsap.from(".SomosQ", {
+    y: '-15%',
+    duration: 2,
+    delay: 1,
+    scrollTrigger: ".SomosQ"
+})
+
+gsap.from(".Productos", {
+    y: '-15%',
+    duration: 2,
+    delay: 1,
+    scrollTrigger: ".Productos"
+})
+
+gsap.from(".Contacto", {
+    y: '-15%',
+    duration: 2,
+    delay: 1,
+    scrollTrigger: ".Contacto"
+})
+
+
+
+
+function Enviar() {
+    var Nombre = document.getElementById("InputNombre").value;
+    var Teléfono = document.getElementById("InputTelefono").value;
+    var Email = document.getElementById("InputEmail").value;
+    var Mensaje = document.getElementById("InputMensaje").value;
+    {
+        console.log("Nombre: " + Nombre);
+        console.log("Email: " + Email);
+        console.log("Teléfono: " + Teléfono);
+        console.log("Mensaje: " + Mensaje);
+
+        alert('Sus datos fueron enviados :D');
+    }
+return;}
