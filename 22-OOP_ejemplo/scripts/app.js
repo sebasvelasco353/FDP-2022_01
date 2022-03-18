501438b5a52d1abfbb3e4fa2053e4abe173a24ee
Sebastian Velasco Borrero <11669998+sebasvelasco353@users.noreply.github.com>
2022-03-18 17:56:24 -0500

    Updated Example

--- 22-OOP_ejemplo/scripts/app.js
+++ 22-OOP_ejemplo/scripts/app.js
@@ -1,37 +1,62 @@
 class Pelota {
-    constructor(posX, posY, speed) {
+    constructor(posX, posY, speed, index) {
         this.posX = posX;
         this.posY = posY;
         this.speed = speed;
+        this.index = index;
+        this.direccion = true;
     }
     move() {
-        if (this.dir()) {
+        this.dir();
+        if (this.direccion) {
             this.posX += this.speed;
         } else {
             this.posX -= this.speed;
         }
     }
     dir() {
-        // TODO: tenemos que completar el movimiento de rebote... como lo hacemos?
-        if (this.posX <= windowWidth && this.posX >= 0) return true;
-        return false;
+        if (this.posX > windowWidth) this.direccion = !this.direccion;
+        if (this.posX < 0) this.direccion = !this.direccion;
+
+    }
+    setDireccion() {
+        this.direccion = !this.direccion;
     }
     draw() {
         ellipse(this.posX, this.posY, 50, 50);
     }
-};
+}
+const PELOTAS = [];
 
-// TODO: como hago si quiero tener muchas, por ejemplo 150?
-// TODO: necesito que cuando la pelota se golpee con una pared diga "Auch!"
+for (let index = 0; index < 2; index++) {
+    console.log(index);
+    PELOTAS.push(new Pelota(randomIntFromInterval(0, window.innerWidth), 
+    300, 10, index));
+}
 
-const ball = new Pelota(100, 100, 10);
+console.log(PELOTAS);
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 function draw() {
     background(220);
-    ball.draw();
-    ball.move();
+    for (let i = 0; i < PELOTAS.length; i++) {
+        const elementMain = PELOTAS[i];
+        elementMain.draw();
+        elementMain.move();
+        for (let j = 0; j < PELOTAS.length; j++) {
+            const element = PELOTAS[j];
+            console.log(`${elementMain.posX} && ${element.posX}`)
+            if ((elementMain.posX === element.posX) && (elementMain.index !== element.index)) {
+                elementMain.setDireccion();
+                console.log("SE ME CHOCARON");
+            }
+        }
+    }
 }
