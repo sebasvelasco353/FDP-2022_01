310615f895664f2bcf6774e1411c2ef8c6624942
FOOSSIILL <sofia3472@outlook.com>
2022-04-01 17:40:27 -0500

    main

--- 24-ejemploAsync/Sofía Motta/app.js
+++ 24-ejemploAsync/Sofía Motta/app.js
@@ -1,67 +1,20 @@
-const url = 'https://swapi.dev/api/people';
-const loading = document.getElementById('loading');
-const filterSelect = document.getElementById('filter');
-const reducerSelect = document.getElementById('reducer');
-const reducer_result = document.getElementById('reducer_result');
-const display = document.getElementById('display');
+var url = 'https://swapi.dev/api/people';
+// TODO: get the html element
 
-let results = [];
-let people;
+const datos= document.getElementById("Inicio")
 
-async function run(){
-    const response = await fetch(url);
-    const responseObj = await response.json();
-    people = responseObj.results;
+ async function run(){
+    let response = await fetch(url);
+    let people = await response.json();
 
-   results = filterBy(filterSelect.value);
-   display_data();
-   display_reducer();
+    console.log(people.results);
+    // TODO: use the results to display the author's informatio
 
-   loading.style.display = 'none';   
-}
-
-function display_data(){
-    display.innerHTML = '';
-    results.forEach(element => {
-        var opt = document.createElement('p');
-        opt.className = "display_element"
-        opt.innerHTML = `<p>${element.name}</p> mass: ${element.mass} height: ${element.height}`; 
-        display.appendChild(opt);
-    });
-}
-
-function display_reducer() {
-    reducer_result.innerText = `El resultado del reducer es ${reduce(reducerSelect.value)}`
-}
-
-function filterBy(criteria) {
-    const filters = {
-        'all': people,
-        'height>150': people.filter(p => parseInt(p.height) > 150),
-        'height<150': people.filter(p => parseInt(p.height)<150),
-        'mass<100': people.filter(p => parseInt(p.mass) < 100),
-        'mass>100': people.filter(p => parseInt(p.mass) > 100),
+    for (let i = 0; i < people.results.length ; i++) {
+        console.log("Genero:", people.results[i].gender)
+        console.log("Masa:", people.results[i].mass)
+        console.log("Color de ojos:",people.results[i].eye_color)
+        datos.innerHTML = datos.innerHTML + `<p>` + `${people.results[i].hair_color}` + `</p>`;
     }
-    return filters[criteria];
 }
-
-function reduce(criteria) {
-    console.log(results); 
-    const reducers = {
-        'heightSum': results.reduce((a, b) => parseInt(a.height) + parseInt(b.height)),
-        'massSum': results.reduce((a, b) => parseInt(a.mass) + parseInt(b.mass)),
-    } 
-    return reducers[criteria];
-}
-
-filterSelect.addEventListener("change", (e) => {
-    results = filterBy(filterSelect.value);
-    display_data();
-    display_reducer();
-});
-
-reducerSelect.addEventListener("change", (e) => {
-    display_reducer();
-});
-
 run();
