c5e904cc855682cf529ec5b9e461956d62fef4aa
FOOSSIILL <sofia3472@outlook.com>
2022-04-01 16:26:20 -0500

    my request

--- 24-ejemploAsync/app.js
+++ 24-ejemploAsync/app.js
@@ -1,11 +1,67 @@
-var url = 'https://swapi.dev/api/people';
-// TODO: get the html element
+const url = 'https://swapi.dev/api/people';
+const loading = document.getElementById('loading');
+const filterSelect = document.getElementById('filter');
+const reducerSelect = document.getElementById('reducer');
+const reducer_result = document.getElementById('reducer_result');
+const display = document.getElementById('display');
 
-function run(){
-    let response = await fetch(url);
-    let people = response.json();
+let results = [];
+let people;
 
-    console.log(people.results);
-    // TODO: use the results to display the author's information.
+async function run(){
+    const response = await fetch(url);
+    const responseObj = await response.json();
+    people = responseObj.results;
+
+   results = filterBy(filterSelect.value);
+   display_data();
+   display_reducer();
+
+   loading.style.display = 'none';   
 }
+
+function display_data(){
+    display.innerHTML = '';
+    results.forEach(element => {
+        var opt = document.createElement('p');
+        opt.className = "display_element"
+        opt.innerHTML = `<p>${element.name}</p> mass: ${element.mass} height: ${element.height}`; 
+        display.appendChild(opt);
+    });
+}
+
+function display_reducer() {
+    reducer_result.innerText = `El resultado del reducer es ${reduce(reducerSelect.value)}`
+}
+
+function filterBy(criteria) {
+    const filters = {
+        'all': people,
+        'height>150': people.filter(p => parseInt(p.height) > 150),
+        'height<150': people.filter(p => parseInt(p.height)<150),
+        'mass<100': people.filter(p => parseInt(p.mass) < 100),
+        'mass>100': people.filter(p => parseInt(p.mass) > 100),
+    }
+    return filters[criteria];
+}
+
+function reduce(criteria) {
+    console.log(results); 
+    const reducers = {
+        'heightSum': results.reduce((a, b) => parseInt(a.height) + parseInt(b.height)),
+        'massSum': results.reduce((a, b) => parseInt(a.mass) + parseInt(b.mass)),
+    } 
+    return reducers[criteria];
+}
+
+filterSelect.addEventListener("change", (e) => {
+    results = filterBy(filterSelect.value);
+    display_data();
+    display_reducer();
+});
+
+reducerSelect.addEventListener("change", (e) => {
+    display_reducer();
+});
+
 run();
