0eb9c9319a157e4dfb425063dd31a641c41030f6
Kathereyes19 <98465512+Kathereyes19@users.noreply.github.com>
2022-04-18 07:35:11 -0500

    Add files via upload

--- 24-ejemploAsync/Katherine Reyes/app.js
+++ 24-ejemploAsync/Katherine Reyes/app.js
@@ -1,23 +1,27 @@
 var url = 'https://swapi.dev/api/people';
-// TODO: get the html element
 
-const inyectardato1 = document.getElementById("character1");
-const inyectardato2 = document.getElementById("character2");
-const inyectardato3 = document.getElementById("character3");
-
-async function run(){
+async function Run(){
     let response = await fetch(url);
-    let people = response.json();
+    let people = await response.json();
 
     console.log(people.results);
-    // TODO: use the results to display the author's information.
 
-    for (let i = 0; i < people.results.length; i++){
-        console.log(people.results[i].name)
-        inyectardato1.innerHTML = inyectardato1.innerHTML + "<p>" + "${people.results[i].name}" + "<p>";
-        inyectardato2.innerHTML = inyectardato2.innerHTML + "<p>" + "${people.results[i].skincolor}" + "<p>";
-        inyectardato2.innerHTML = inyectardato3.innerHTML + "<p>" + "${people.results[i].gender}" + "<p>";
-    }
+    people.results.forEach(e => {
+        console.log({
+            name: e.name,
+            gender: e.gender,
+            height: e.height,
+            mass: e.mass
+        })
+
+        let Info = document.getElementById('Content');
+
+        let DisplayInfo = document.createElement('p');
+        DisplayInfo.innerHTML = e.name + "'s information is:" + `<br>` + "Gender: " + e.gender
+        + `<br>` + "Height: " + e.height + `<br>` + "Mass: " + e.mass;
+        Info.appendChild(DisplayInfo);
+
+    });
 }
 
-run();
+Run();
