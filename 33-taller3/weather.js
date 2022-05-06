e8636cadc0532ac29287a256f3fd5a27bf2916c9
Sebastian Velasco Borrero <11669998+sebasvelasco353@users.noreply.github.com>
2022-05-06 16:42:14 -0500

    Update weather.js

--- 33-taller3/weather.js
+++ 33-taller3/weather.js
@@ -7,49 +7,49 @@
                 {
                     "day": "monday",
                     "celcius": 20,
-                    "precipitation%": 60,
+                    "precipitation": 60,
                     "uvIndex": 2,
                     "sunny": false
                 },
                 {
                     "day": "tuesday",
                     "celcius": 25,
-                    "precipitation%": 0,
+                    "precipitation": 0,
                     "uvIndex": 5,
                     "sunny": true
                 },
                 {
                     "day": "wednesday",
                     "celcius": 30,
-                    "precipitation%": 40,
+                    "precipitation": 40,
                     "uvIndex": 3,
                     "sunny": false
                 },
                 {
                     "day": "thursday",
                     "celcius": 15,
-                    "precipitation%": 100,
+                    "precipitation": 100,
                     "uvIndex": 1,
                     "sunny": false
                 },
                 {
                     "day": "fiday",
                     "celcius": 28,
-                    "precipitation%": 10,
+                    "precipitation": 10,
                     "uvIndex": 3,
                     "sunny": true
                 },
                 {
                     "day": "saturday",
                     "celcius": 25,
-                    "precipitation%": 20,
+                    "precipitation": 20,
                     "uvIndex": 2,
                     "sunny": false
                 },
                 {
                     "day": "sunday",
                     "celcius": 20,
-                    "precipitation%": 20,
+                    "precipitation": 20,
                     "uvIndex": 2,
                     "sunny": true
                 }
@@ -61,49 +61,49 @@
                 {
                     "day": "monday",
                     "celcius": 10,
-                    "precipitation%": 60,
+                    "precipitation": 60,
                     "uvIndex": 2,
                     "sunny": false
                 },
                 {
                     "day": "tuesday",
                     "celcius": 15,
-                    "precipitation%": 0,
+                    "precipitation": 0,
                     "uvIndex": 5,
                     "sunny": true
                 },
                 {
                     "day": "wednesday",
                     "celcius": 10,
-                    "precipitation%": 40,
+                    "precipitation": 40,
                     "uvIndex": 3,
                     "sunny": false
                 },
                 {
                     "day": "thursday",
                     "celcius": 13,
-                    "precipitation%": 100,
+                    "precipitation": 100,
                     "uvIndex": 1,
                     "sunny": false
                 },
                 {
                     "day": "fiday",
                     "celcius": 18,
-                    "precipitation%": 10,
+                    "precipitation": 10,
                     "uvIndex": 3,
                     "sunny": true
                 },
                 {
                     "day": "saturday",
                     "celcius": 15,
-                    "precipitation%": 20,
+                    "precipitation": 20,
                     "uvIndex": 2,
                     "sunny": false
                 },
                 {
                     "day": "sunday",
                     "celcius": 10,
-                    "precipitation%": 20,
+                    "precipitation": 20,
                     "uvIndex": 2,
                     "sunny": true
                 }
