2f98d717b93ddbaac8be031f93b79e18faab1070
DarkSea1220 <98364095+Darksea1220@users.noreply.github.com>
2022-05-06 15:52:25 -0500

    Pues, pude separar los arrays y hacerlos aparecer, queremos hablar contigo profe, sabemos que no hemos sido los mejores estudiantes, pero por favor escúchanos

--- 33-taller3/Douglas Osorio/douglas2.js
+++ 33-taller3/Douglas Osorio/douglas2.js
@@ -1,6 +1,7 @@
 const url = 'https://raw.githubusercontent.com/sebasvelasco353/FDP-2022_01/main/33-taller3/weather.js';
 const resultsCont = document.getElementById("container");
 const filtrosemana = document.getElementById("dias");
+const filtrocity = document.getElementById("maincitys")
 let dias;
 let CIUDADES=[];
 let DIARIO=[];
@@ -18,30 +19,50 @@ async function run(){
     const response = await fetch(url);
     const responseObj = await response.json();
     dias = responseObj.ciudades;
+    sacardaily(dias);
     for (let i = 0; i < dias.length; i++) {
         CIUDADES.push(
             new ciudades(dias[i].city, dias[i].weeklyOverall, dias[i].daily)
         );
-        console.log(CIUDADES);
-        DIARIO.push(this.semana);
     }
-    console.log(DIARIO);
-    semanaCali.push(DIARIO[0])
-    semanaBogota.push(DIARIO[1])
-    // console.log(semanaBogota)
-    // console.log(semanaCali)
-    //set_cali();
-    //set_bogota();
+    semanaCali.push(DIARIO.slice(2,37))
+    semanaBogota.push(DIARIO.slice(39)) 
+    console.log(semanaCali);
+    console.log(semanaBogota);
+    set();
 }
-// function set_cali() {
-//     resultsCont.innerHTML = '';
-//     semanaCali.forEach(element => {
-//         element.forEach(anotherel =>{
-//         var elm = document.createElement('div');
-//         elm.className = "display_day"
-//         elm.innerHTML = `${anotherel.day} ${anotherel.celcius} ${anotherel.uvIndex} ${anotherel.sunny}`;
-//         resultsCont.appendChild(elm);
-//         })
+function set (){
+    if (filtrocity.value === "cali") {
+        set_cali();
+    }else if (filtrocity.value === "bogota"){
+        set_bogota();
+    }
+}
+filtrocity.addEventListener("change", (e) =>{
+set();
+});
+function set_cali() {
+    resultsCont.innerHTML = '';
+    for (let i = 0; i < semanaCali.length; i++) {
+        var elm = document.createElement('div');
+        elm.className = "display_day"
+        elm.innerHTML = `${semanaCali[i]}`;
+        resultsCont.appendChild(elm);
+    }    
+}
+function set_bogota() {
+    resultsCont.innerHTML = '';
+    for (let i = 0; i < semanaBogota.length; i++) {
+        var elm = document.createElement('div');
+        elm.className = "display_day"
+        elm.innerHTML = `${semanaBogota[i]}`;
+        resultsCont.appendChild(elm);
+    }}    
+// semanaCali.forEach(element => {
+    //     var elm = document.createElement('div');
+    //     elm.className = "display_day"
+    //     elm.innerHTML = `${element[0]}`;
+    //     resultsCont.appendChild(elm);
 //     });
 // }
 // function set_bogota() {
@@ -55,4 +76,16 @@ async function run(){
 //         })
 //     });
 // }
+function sacardaily(data) {
+    for (var key in data) {
+      if (typeof(data[key]) == "object" && data[key] != null) {
+        sacardaily(data[key]);
+      } else {
+        DIARIO.push( key + " = " + data[key]);
+      }
+    }
+} 
+function colordeldia (){
+
+}
 run();
