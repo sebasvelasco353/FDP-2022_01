0931e87ff51837cf6d47a81397b8e3b3b6119d9c
Kathereyes19 <98465512+Kathereyes19@users.noreply.github.com>
2022-05-09 08:44:57 -0500

    CAMBIOS FINALES- ENTREGA
    trabajo terminado cumpliendo los requerimientos

--- 33-taller3/Katherine Reyes Taller 3/app.js
+++ 33-taller3/Katherine Reyes Taller 3/app.js
@@ -1,29 +1,11 @@
 const URL = 'https://raw.githubusercontent.com/sebasvelasco353/FDP-2022_01/main/33-taller3/weather.js';
-const DAYS = [];
-const domElement = document.getElementById("element");
+
+const DayCards = document.getElementById("Day");
+const Overall = document.getElementById("Overall");
+
+let DAYS = [];
 let theData;
 
-class Day {
-    constructor(day, temp, precipitation, uvIndex, sunny, city) {
-        this.day = day;
-        this.temp = temp;
-        this.precipitation = precipitation;
-        this.uv = uvIndex;
-        this.sunny = sunny;
-        this.city = city;
-    }
-    
-    draw(elm) {
-        const opt = document.createElement('div');
-        if (this.sunny) {
-            opt.className = "display_element sunny";
-        } else {
-            opt.className = "display_element rainy";
-        }
-        opt.innerHTML = `<p><b class="tittle">${this.city} ${this.day} ${this.sunny ? "Soleado" : "Lloviendo"}</b></p> <p class="text"> Temperatura: ${this.temp}ºC Precipitación: ${this.precipitation} Indice UV: ${this.uv}</p>`;
-        elm.appendChild(opt);
-    }
-}
 window.onload = function() {
     fetch(URL)
     .then(response => response.json())
@@ -35,29 +17,71 @@ window.onload = function() {
 
 function processData(data) {
     data.ciudades.forEach(ciudad => {
+        console.log(ciudad);
         ciudad.daily.forEach(dayObj => {
-            DAYS.push(new Day(dayObj.day, dayObj.celcius, dayObj.precipitation, dayObj.uvIndex, dayObj.sunny, ciudad.city));
+            DAYS.push(new Day(dayObj.day, dayObj.celcius, dayObj.precipitation, dayObj.uvIndex, dayObj.sunny, ciudad.city, ciudad.weeklyOverall));
         });
     });
     drawElements();
 }
 
+class Day {
+    constructor(day, temp, precipitation, uvIndex, sunny, city, weeklyOverall) {
+        this.day = day;
+        this.temp = temp;
+        this.precipitation = precipitation;
+        this.uv = uvIndex;
+        this.sunny = sunny;
+        this.city = city;
+        this.weekly = weeklyOverall;
+        }
+    
+    drawCards(elm) {
+        const opt = document.createElement('div');
+        if (this.sunny) {
+            opt.className = "elementSunny";
+        } else {
+            opt.className = "elementRainy";
+        }
+        opt.innerHTML = `<p> <span class="Tittle">${this.day}, <br> ${this.city}</span> <br> <span class="textTemp">${this.temp}°C </span> <br> <span class = "textStatus">${this.sunny ? "Soleado" : "Lluvioso"}</span> <br> Precipitation: ${this.precipitation}% <br> ${this.uv} Uv</p>`;        
+        elm.appendChild(opt);
+    }
+
+d_overall(overall) {
+    const opt = document.createElement('div');
+    if (this.sunny) {
+        opt.className = "overallsunny";
+    } else {
+        opt.className = "overallRainy";
+    }
+    opt.innerHTML = `El clima estara <span>${this.weekly}</span> en <span>${this.city}</span> esta semana`;
+    overall.appendChild(opt);
+}
+}
+
 function drawElements() {
-    domElement.innerHTML = "";
+    DayCards.innerHTML = "";
+    Overall.innerHTML = "";
     const valueC= document.getElementById("select").value;
-    const results = DAYS.filter(day => day.city === valueC);
-    console.log(results);
-
+    const resultsC = DAYS.filter(day => day.city === valueC);
     const valueD = document.getElementById("daysSelector").value;
-    console.log(valueD);
 
-    const D =results.filter(day => day.day ===valueD);
-    for (let i =D.length-1; i >= 0; i--) {
-        const element = D[i];
-        element.draw(domElement);
+    for (let i = 0; i < resultsC.length - 6; i++) {
+        const element = resultsC[i];
+        element.d_overall(Overall);
+    }
+    if (valueD === "todos") {
+        for (let i = 0; i < resultsC.length; i++) {
+            const element = resultsC[i];
+            element.drawCards(DayCards);
+        }
+    } else {
+        const resultsD = resultsC.filter(day => day.day === valueD);
+        const element = resultsD[0];
+        element.drawCards(DayCards);
     }
 }
-//ANIMACIONES
+//ANIMACIONES´
 gsap.from('.nav-bar', { duration: 1, x: '-100%', ease: 'power3.inOut', delay: 0.2})
 gsap.from('.Inicio', { duration: 2, delay: 0.1, x: '-100%', opacity: '0', ease: 'power3.inOut'})
 
