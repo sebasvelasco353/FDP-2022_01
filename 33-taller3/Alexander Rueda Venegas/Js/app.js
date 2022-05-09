06be045b5ea8e2dc35bbffe5903c6d530207adc0
Alexander <98567345+Axelito02@users.noreply.github.com>
2022-05-09 00:14:30 -0500

    avances del taller 3

--- 33-taller3/Alexander Rueda Venegas/Js/app.js
+++ 33-taller3/Alexander Rueda Venegas/Js/app.js
@@ -1,33 +1,85 @@
 const URL_api = "https://raw.githubusercontent.com/Axelito02/FDP-2022_01/main/33-taller3/weather.js";
 
+// search bar
 const search_cities = document.getElementById('bar_search');
-const display_temperature = document.querySelector('#cards_temperature');
-const display_precipation = document.querySelector('#cards_precipation');
-const display_uvlevel = document.querySelector('#cards_levelUV');
-const display_sunny = document.querySelector('#cards_sunny');
+
+// containers to display the data
+const display_temperature = document.querySelector('#content_temperature');
+const display_precipation = document.querySelector('#content_precipitation');
+const display_uvlevel = document.querySelector('#content_ultraviolet');
+const display_sunny = document.querySelector('#content_sunny');
+const display_city_title = document.querySelector('#ciudad_tilte');
 
 let api;
 let city;
-let days;
 
 async function run(){
-    const content_api = await fetch(URL_api);
-    api = await content_api.json();
-    api.ciudades.forEach(e => {
-
-        if(e.city === search_cities.value){
-            city=e
-        }
-
-    });
-
-    if(api.ciudades[0].daily[1].sunny){
-        display_temperature.className = "cards--sunny"
-    }
-    
-    console.log(city.city);
+    const res = await fetch(URL_api);
+    api = await res.json();
+    city = api.ciudades;
+    results = search_cities.value;
+    draw();
+    console.log(city);
 }
 
-if(city.daily[0].sunny){
-    display_temperature.className = ""    
+// class day_of_the_week {
+//     constructor(day, temp, sunny) {
+//         this.day = day;
+//         this.temp = temp;
+//         this.sunny = sunny;
+//     }
+//     draw(domObj){
+//         console.log(this.day, this.temo, this.sunny);
+//     }
+// }
+
+// funtion for get the object bogota
+function draw(){
+    city.forEach(city => {
+        const opt = document.createElement('div');
+        opt.className = "display_element";
+        opt.innerHTML = `<p><b>${city.daily[0].celcius+" "+"degrees Celsius"}`;
+        display_temperature.appendChild(opt);
+    });
+
+    city.forEach(city => {
+        const opt = document.createElement('div');
+        opt.className = "display_element";
+        opt.innerHTML = `<p><b>${city.daily[0].precipitation+"%"}`;
+        display_precipation.appendChild(opt);
+    });
+
+    city.forEach(city => {
+        const opt = document.createElement('div');
+        opt.className = "display_element";
+        opt.innerHTML = `<p><b>${"uv index"+" "+city.daily[0].uvIndex}`;
+        display_uvlevel.appendChild(opt);
+    });
+
+    city.forEach(city => {
+        const opt = document.createElement('div');
+        opt.className = "display_element";
+        opt.innerHTML = `<p><b>${city.daily[0].sunny}`;
+        display_sunny.appendChild(opt);
+    });
+
+    city.forEach(city => {
+        const opt = document.createElement('div');
+        opt.className = "display_element";
+        opt.innerHTML = `<p><b>${city.city}`;
+        display_city_title.appendChild(opt);
+    });
+
+}
+
+function clear_mo() {
+    display_temperature.innerHTML = "";
+    display_precipation.innerHTML = "";
+    display_uvlevel.innerHTML = "";
+    display_sunny.innerHTML = "";
+    display_city_title.innerHTML = "";
+    gsap.from('#content_temperature', { duration: 1, y: '-120%', ease: 'power3.inOut', delay: 0.2})
+    gsap.from('#display_precipation', { duration: 1, y: '-120%', ease: 'power3.inOut', delay: 0.2})
+    gsap.from('#display_sunny', { duration: 1, y: '-120%', ease: 'power3.inOut', delay: 0.2})
+    gsap.from('#display_uvlevel', { duration: 1, y: '-120%', ease: 'power3.inOut', delay: 0.2})
 }
