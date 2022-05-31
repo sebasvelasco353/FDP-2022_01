a7eefb4fd1afeb3eea5e508e0dcb459283a40ffb
Alexander <98567345+Axelito02@users.noreply.github.com>
2022-05-31 01:28:33 -0500

    juego casi Bustercall Game casi terminado

--- 35-FINAL/Alexander and Alejandro/Js/map/map.js
+++ 35-FINAL/Alexander and Alejandro/Js/map/map.js
@@ -6,6 +6,11 @@ const ctx = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const point = document.querySelector('#point');
+const startGame = document.querySelector('#StartGame');
+const containerScore = document.querySelector('#background');
+const endPoint= document.querySelector('#scoretEnd');
+
 // object player
 class Player{
     constructor(x, y, radius, color){
@@ -25,7 +30,19 @@ class Player{
 const x = canvas.width / 2; 
 const y = canvas.height / 2;
 
-const player = new Player (x, y, 32, 'darkblue');
+
+// arrays 
+let player = new Player (x, y, 24, 'white');
+let projectiles = [];
+let enemies = [];
+
+function resetGame(){
+    player = new Player (x, y, 24, 'white');
+    projectiles = [];
+    enemies = [];
+    score = 0;
+    point.innerHTML = score;
+}
 
 // object bullet
 class Projectile{
@@ -69,11 +86,8 @@ class Enemy{
         this.x = this.x + this.velocity.x,
         this.y = this.y + this.velocity.y
     }
+    
 };
-
-// arrays bullet and enemies
-const projectiles = [];
-const enemies = [];
 
 // spawnEnemies
 function spawnEnemies(){
@@ -101,41 +115,88 @@ function spawnEnemies(){
 };
 
 // animation projectile
+let idAnimation;
+let score = 0;
+
 function animation(){
-    requestAnimationFrame(animation)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    player.draw()
-    projectiles.forEach((projectile) => {
+    idAnimation = requestAnimationFrame(animation)
+    ctx.fillStyle= 'rgba(0, 0, 0, 0.4)';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+    player.draw();
+
+    projectiles.forEach((projectile, index) => {
         projectile.update();
+
+        // end the game
+        if(projectile.x + projectile.radius < 0 ||
+           projectile.x - projectile > canvas.width ||
+           projectile.y + projectile < 0 ||
+           projectile.y - projectile > canvas.height){
+            setTimeout(() => {
+                projectiles.splice(index, 1);
+            }, 0);
+        }
     })
 
+// condition to hit the enemy and player
         enemies.forEach((enemy, index) => {
-         enemy.update();
+            enemy.update();
+         const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+            
+        //  end game
+         if (dist - enemy.radius - player.radius < 1){
+            cancelAnimationFrame(idAnimation);
+            containerScore.style.display = 'flex';
+            endPoint.innerHTML = score;
+         }
 
-// condition to hit the bullet and the enemy
+        // condition to hit the bullet and the enemy
         projectiles.forEach((projectile, projectileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
             if (dist - enemy.radius - projectile.radius < 1)
-            enemies.splice(index, 1);
-            projectiles.splice(projectileIndex, 1);
+            {
+               if(enemy.radius - 12 > 6){
+                    // increase points
+                    score += 100;
+                    point.innerHTML = score;
+
+                    setTimeout(() => {
+                        enemies.splice(index, 1);
+                        projectiles.splice(projectileIndex, 1);
+                    }, 0);
+                } else{
+                    score += 400;
+                    point.innerHTML = score;
+                    setTimeout(() => {
+                        enemies.splice(index, 1);
+                        projectiles.splice(projectileIndex, 1);
+                    }, 0);
+                }
+            }
         })
     })
 };
 
+// event click the bullet
 addEventListener('click', (event) =>{
     const angle = Math.atan2(event.clientX - canvas.width / 2, event.clientY - canvas.height / 2);
     const velocity = {
-        x: Math.sin(angle),
-        y: Math.cos(angle)
+        x: Math.sin(angle) * 3,
+        y: Math.cos(angle) * 3,
     }
     projectiles.push(new Projectile(
         canvas.width / 2,
         canvas.height / 2,
         8,
-        'lightblue',
+        'blue',
         velocity
     ))
 });
 
-animation();
-spawnEnemies();
+startGame.addEventListener('click', () =>{
+    resetGame()
+    animation();
+    spawnEnemies();
+    containerScore.style.display = 'none';
+});
+
