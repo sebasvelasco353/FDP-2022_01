d98a98e27b722e51404a7050aab13c92720590c3
Kazura-Collins <98463668+Kazura-Collins@users.noreply.github.com>
2022-05-31 19:56:39 -0500

    Entrega final
    página fuertemente basada en la landing page, pero con temática distinta y llamando nombres de api del taller 3.

--- 35-FINAL/Trabajo Final Isaac Montaño/app.js
+++ 35-FINAL/Trabajo Final Isaac Montaño/app.js
@@ -1,1 +1,30 @@
-//Uso del json
+//Api
+var url = 'https://swapi.dev/api/people';
+
+let people;
+
+async function run(){
+    let response = await fetch(url);
+    people = await response.json();
+
+    people.results.forEach(element => {
+        console.log({
+            name: element.name
+        });
+    });
+    
+    display_data();
+}
+
+function display_data() {
+    const display = document.getElementById("display");
+    display.innerHTML = '';
+    people.results.forEach(element => {
+        var opt = document.createElement('p');
+        opt.className = "display_element"
+        opt.innerHTML = `<p><b><font size="3">${element.name}</font></b></p>`;
+        display.appendChild(opt);
+    });
+}
+
+run();
